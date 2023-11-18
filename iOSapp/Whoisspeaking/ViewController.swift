4f977524869f8de59cfef891bf164fb88469a0cb
rongwei <rongwei.ji@outlook.com>
2023-11-18 13:57:09 -0600

    init the branch to desgin the main ue

--- iOSapp/Whoisspeaking/ViewController.swift
+++ iOSapp/Whoisspeaking/ViewController.swift
@@ -4,6 +4,11 @@
 //
 //  Created by RongWei Ji on 11/18/23.
 //
+// here is main logic for the ui
+// 1. open app directly dect speaking ,and send to server to anay
+//    - no record on server ,returen infor -directly to train who you are
+//
+//    - yes record return who is speaking
 
 import UIKit
 
