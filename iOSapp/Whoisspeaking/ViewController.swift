4f3b6ec505a9121175de2598b1785c8fa5af126a
rongwei <rongwei.ji@outlook.com>
2023-11-18 13:51:28 -0600

    add xcode project

---
+++ iOSapp/Whoisspeaking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whoisspeaking
+//
+//  Created by RongWei Ji on 11/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
